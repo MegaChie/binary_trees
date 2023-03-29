d3d406336831e56afba4428aae8fae78c36bb7f0
MegaChie <workspace.mega@gmail.com>
2023-03-29 17:39:01 +0200

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		size_t leftey = 0, rightey = 0;
 
 		leftey = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		rightey = tree->right ? 1 + binary_tree_height(tree->right) : 0;\
+		rightey = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((leftey > r) ? leftey : rightey);
 	}
 	return (0);
