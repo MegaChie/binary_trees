a7e166408c9b069835c1837f41a4206b2d3ec851
MegaChie <workspace.mega@gmail.com>
2023-03-29 11:49:49 +0200

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,6 +14,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	}
+	new = binary_tree_node(parent, value);
 	if (parent->right != 0)
 	{
 		new->right = parent->right;
