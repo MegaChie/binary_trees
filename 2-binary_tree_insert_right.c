f77117c28e89602f7679777d16a4792284bcad53
MegaChie <workspace.mega@gmail.com>
2023-03-29 11:48:00 +0200

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,6 +3,7 @@
  * binary_tree_insert_right - check code.
  * @parent: structure pointer
  * @value: integer variable
+ * Return: NULL or pointer
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
