b29be5be265022f076c618b5b9c885884864666a
MegaChie <workspace.mega@gmail.com>
2023-03-31 01:21:15 +0200

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,13 +6,14 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == 0 || node->parent == 0 || node->parent->parent == 0)
+if (node == 0 || node->parent == 0 || node->parent->parent == 0)
     {
         return (0);
     }
-    if (node->parent->parent->parent->left == node->parent)
+    if (node->parent->parent->left == node->parent)
     {
         return (node->parent->parent->right);
     }
     return (node->parent->parent->left);
+
 }
