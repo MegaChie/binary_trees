bea7c68e7c809b9c04b7cdbfdb313c544b702422
MegaChie <workspace.mega@gmail.com>
2023-03-31 01:09:40 +0200

    Create 17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - check code.
+ * @node: constant structure pointer
+ * Return: 0 or function's value
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    if (node == 0 || node->parent == 0)   
+    {
+        return (0);
+    }
+    if (node->parent->left == node)
+    {
+        return (node->parent->right);
+    }
+    return (node->parent->left);
+}
