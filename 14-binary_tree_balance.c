2129593976129dfa20f73c694e2a5f42ac6777f1
MegaChie <workspace.mega@gmail.com>
2023-03-30 03:20:12 +0200

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,9 +24,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t leftey = 0, rightey = 0;
 
-		leftey = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		rightey = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((leftey > rightey) ? leftey : rightey);
+		leftey = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rightey = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((leftey > rightey) ? 1 : rightey);
 	}
 	return (0);
 }
